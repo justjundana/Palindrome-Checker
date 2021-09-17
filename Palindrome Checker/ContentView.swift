ece62ec56210b2fe0d78ab7dac440273965d4355
justjundana <jalbasyir@gmail.com>
2021-09-17 08:30:20 +0700

    Initial Commit

---
+++ Palindrome Checker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Palindrome Checker
+//
+//  Created by Jundana Al-Basyir on 17/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Palindrome Checker/Palindrome_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Palindrome_CheckerApp.swift
+//  Palindrome Checker
+//
+//  Created by Jundana Al-Basyir on 17/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Palindrome_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
